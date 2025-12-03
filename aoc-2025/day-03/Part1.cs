69125b7acd500546806d303cd24c96d64dbf8b0a
Perry Lynch <perryklynch@gmail.com>
2025-12-03 07:36:42 -0500

    Day 03

---
+++ aoc-2025/day-03/Part1.cs
@@ -0,0 +1,42 @@
+﻿internal sealed class Part1
+{
+	public static void Run()
+	{
+		var sum = 0;
+
+		var sr = new StreamReader("input.txt");
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			sum += GetJoltage(line);
+			line = sr.ReadLine();
+		}
+
+		Console.WriteLine(sum);
+	}
+
+	private static int GetJoltage(string line)
+	{
+		var greatestDigitFound = 0;
+		var greatestSecondDigitFound = 0;
+		var numValues = line.Length;
+		for (var i = 0; i < numValues; i++)
+		{
+			var v = (int)(line[i] - '0');
+			if (i < numValues - 1)
+			{
+				if (v > greatestDigitFound)
+				{
+					greatestDigitFound = v;
+					greatestSecondDigitFound = 0;
+				}
+				else if (v > greatestSecondDigitFound)
+					greatestSecondDigitFound = v;
+			}
+			else if (v > greatestSecondDigitFound)
+				greatestSecondDigitFound = v;
+		}
+
+		return greatestDigitFound * 10 + greatestSecondDigitFound;
+	}
+}
---
+++ aoc-2025/day-03/Part2.cs
@@ -0,0 +1,53 @@
+﻿internal sealed class Part2
+{
+	public static void Run()
+	{
+		var sum = 0L;
+
+		var sr = new StreamReader("input.txt");
+		var line = sr.ReadLine();
+		while (!string.IsNullOrEmpty(line))
+		{
+			sum += GetJoltage(line);
+			line = sr.ReadLine();
+		}
+
+		Console.WriteLine(sum); // 172681562473501
+	}
+
+	private static long GetJoltage(string line)
+	{
+		var numDigits = 12;
+		var digits = new int[numDigits];
+
+		var numValues = line.Length;
+		for (var i = 0; i < numValues; i++)
+		{
+			var v = line[i] - '0';
+
+			// digits is sorted descending, could do a log(N) search for the index to set v.
+			// But numDigits is small and this is simple.
+
+			for (var di = 0; di < numDigits; di++)
+			{
+				if (v > digits[di] && numDigits - di <= (numValues - i))
+				{
+					digits[di] = v;
+					for (var ri = di + 1; ri < numDigits; ri++)
+						digits[ri] = 0;
+					break;
+				}
+			}
+		}
+
+		var result = 0L;
+		var mult = 1L;
+		for (var i = numDigits - 1; i >= 0; i--)
+		{
+			result += digits[i] * mult;
+			mult *= 10;
+		}
+
+		return result;
+	}
+}
