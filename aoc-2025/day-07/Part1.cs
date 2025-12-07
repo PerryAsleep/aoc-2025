047cca62d3f58f938abb85270c59990e0d58628f
Perry Lynch <perryklynch@gmail.com>
2025-12-07 07:48:29 -0500

    Day 07

---
+++ aoc-2025/day-07/Part1.cs
@@ -0,0 +1,39 @@
+﻿internal sealed class Part1
+{
+	public void Run()
+	{
+		var sr = new StreamReader("input.txt");
+		var line = sr.ReadLine();
+		var columnsWithBeams = new HashSet<int>();
+		var numSplits = 0;
+		while (line != null)
+		{
+			for (var x = 0; x < line.Length; x++)
+			{
+				var c = line[x];
+				switch (c)
+				{
+					case 'S':
+					{
+						columnsWithBeams.Add(x);
+						break;
+					}
+					case '^':
+					{
+						if (columnsWithBeams.Contains(x))
+						{
+							numSplits++;
+							columnsWithBeams.Remove(x);
+							columnsWithBeams.Add(x - 1);
+							columnsWithBeams.Add(x + 1);
+						}
+						break;
+					}
+				}
+			}
+			line = sr.ReadLine();
+		}
+
+		Console.WriteLine(numSplits);
+	}
+}
---
+++ aoc-2025/day-07/Part2.cs
@@ -0,0 +1,50 @@
+﻿internal sealed class Part2
+{
+	public void Run()
+	{
+		var sr = new StreamReader("input.txt");
+		var line = sr.ReadLine();
+		var timelinesPerColumns = new Dictionary<int, long>();
+		while (line != null)
+		{
+			for (var x = 0; x < line.Length; x++)
+			{
+				var c = line[x];
+				switch (c)
+				{
+					case 'S':
+					{
+						timelinesPerColumns.Add(x, 1);
+						break;
+					}
+					case '^':
+					{
+						if (timelinesPerColumns.ContainsKey(x))
+						{
+							var timelines = timelinesPerColumns[x];
+							timelinesPerColumns.Remove(x);
+
+							if (!timelinesPerColumns.ContainsKey(x - 1))
+								timelinesPerColumns.Add(x - 1, timelines);
+							else
+								timelinesPerColumns[x - 1] += timelines;
+
+							if (!timelinesPerColumns.ContainsKey(x + 1))
+								timelinesPerColumns.Add(x + 1, timelines);
+							else
+								timelinesPerColumns[x + 1] += timelines;
+						}
+						break;
+					}
+				}
+			}
+			line = sr.ReadLine();
+		}
+
+		var sum = 0L;
+		foreach (var kvp in timelinesPerColumns)
+			sum += kvp.Value;
+
+		Console.WriteLine(sum);
+	}
+}
